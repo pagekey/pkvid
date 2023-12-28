31567dca5600d6b4097d23e289ef632db1676b96
Stephen Grice <stephengrice@live.com>
2023-12-28 12:41:37 -0500

    Add test to get config string from file

--- pkvid/config.py
+++ pkvid/config.py
@@ -1,8 +1,17 @@
 import os
 
 
+def get_file_as_string(filename: str):
+    with open(filename, "r") as file:
+        contents = file.read()
+    return contents
+
 def process_config(filename: str):
     if os.path.exists(filename):
-        print("Processing: " + filename)
+        contents = get_file_as_string(filename)
+        config = parse_config_from_string(contents)
     else:
         raise ValueError(f"File not found: {filename}")
+
+def parse_config_from_string(contents: str):
+    pass
--- test/test_config.py
+++ test/test_config.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 import pytest
 
 from pkvid.config import process_config
@@ -7,3 +8,12 @@ def test_process_config_invalid_file():
     invalid_filename = 'this_file_doesnt_exist'
     with pytest.raises(ValueError):
         process_config(invalid_filename)
+
+@patch('os.path.exists', return_value=True)
+@patch('pkvid.config.get_file_as_string', return_value='hello')
+@patch('pkvid.config.parse_config_from_string', return_value='hello')
+def test_process_config_valid(mock_parse_config_from_string, mock_get_file_as_string, mock_exists):
+    valid_filename = 'this_file_is_valid'
+    process_config(valid_filename)
+    mock_get_file_as_string.assert_called_with(valid_filename)
+    mock_parse_config_from_string.assert_called_with('hello')
