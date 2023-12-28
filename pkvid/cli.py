4698425f168c1dd542b4539347b6c2a339e03443
Stephen Grice <stephengrice@live.com>
2023-12-28 12:30:32 -0500

    Add argparse for filename

--- pkvid/cli.py
+++ pkvid/cli.py
@@ -1,4 +1,16 @@
+import argparse
 
 
-def cli_entry_point():
-    print("Hello world")
+def main():
+    parser = argparse.ArgumentParser(description='Video editing toolkit')
+    parser.add_argument('filename', help='Name of config file')
+    args = parser.parse_args()
+
+    if args.filename:
+        print(f"Processing file: {args.filename}")
+    else:
+        parser.print_usage()
+
+
+if __name__ == "__main__":
+    main()
