6fef681663d99a3a550c741ba1b48f2ceb970f6e
Stephen Grice <stephengrice@live.com>
2024-03-09 18:44:00 -0500

    Add new subcommand

--- pkvid/cli.py
+++ pkvid/cli.py
@@ -1,28 +1,35 @@
 import argparse
-import os
 import sys
 
-import pkvid.blender as blender
-from pkvid.config import get_config
-from pkvid.project import Project
+# from pkvid.config import get_config
+# from pkvid.project import Project
 
 
 def main(cli_args=sys.argv[1:]):
     parser = argparse.ArgumentParser(description='Video editing toolkit')
-    parser.add_argument('filename', help='Name of config file')
+    subparsers = parser.add_subparsers(dest='command')
+
+    new_parser = subparsers.add_parser('new', help='Create a new project')
+
+    render_parser = subparsers.add_parser('render', help='Render a project')
+    render_parser.add_argument('-f', '--filename', help='Name of config file', default='pkvid.yaml')
+
     args = parser.parse_args(cli_args)
 
-    if args.filename:
-        project_config = get_config(args.filename)
-        project = Project(project_config)
-        print(f"Successfully parsed project: {project.config.name}")
-        dirname = os.path.dirname(args.filename)
-        if len(dirname) > 0:
-            # if not already in that dir, go to it
-            os.chdir(os.path.dirname(args.filename))
-        os.makedirs('render', exist_ok=True)
-        os.chdir('render')
-        project.render()
+    if args.command == 'new':
+        print("Create a new project here")
+    elif args.command == 'render':
+        print("render", args.filename)
+        # project_config = get_config(args.filename)
+        # project = Project(project_config)
+        # print(f"Successfully parsed project: {project.config.name}")
+        # dirname = os.path.dirname(args.filename)
+        # if len(dirname) > 0:
+        #     # if not already in that dir, go to it
+        #     os.chdir(os.path.dirname(args.filename))
+        # os.makedirs('render', exist_ok=True)
+        # os.chdir('render')
+        # project.render()
     else:
         parser.print_usage()
 
